c683434f3343c6ad6bb0742a93540b30c3cfe0b0
j.mir <j.mir.prazak@gmail.com>
2021-09-09 18:46:20 +0200

    blank

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,11 @@
 
 cd /home/pi/avatar
 
+xset s off
+xset -dpms
+unclutter -s 1
+sudo unclutter -s 1
+
 while true; do
 	echo "$(date +%H:%M:%S/%d%m%y) ... STARTING LOOP..."
 	node index.js $(cat /boot/avatar)
